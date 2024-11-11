9e6b0311c44b9e8dc0b6b543a452801bba029f3a
YzkLianAi <2942834094@qq.com>
2024-11-11 17:35:54 +0800

    初始化项目结构

---
+++ src/main/java/com/computer/bikesupervision/BikeSupervisionApplication.java
@@ -0,0 +1,13 @@
+package com.computer.bikesupervision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeSupervisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeSupervisionApplication.class, args);
+    }
+
+}
