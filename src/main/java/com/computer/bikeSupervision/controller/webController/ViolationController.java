9982213759c02bd241e0646fccd5c641ab7390d6
YzkLianAi <2942834094@qq.com>
2025-01-18 15:33:15 +0800

    添加违法信息添加及审核功能

--- src/main/java/com/computer/bikeSupervision/controller/webController/ViolationController.java
+++ src/main/java/com/computer/bikeSupervision/controller/webController/ViolationController.java
@@ -1,15 +1,20 @@
 package com.computer.bikeSupervision.controller.webController;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.computer.bikeSupervision.common.BaseContext;
 import com.computer.bikeSupervision.common.Result;
+import com.computer.bikeSupervision.pojo.dto.ViolationAddDto;
+import com.computer.bikeSupervision.pojo.entity.Administrator;
 import com.computer.bikeSupervision.pojo.entity.PageBean;
+import com.computer.bikeSupervision.pojo.entity.Violation;
+import com.computer.bikeSupervision.service.AdministratorService;
 import com.computer.bikeSupervision.service.ViolationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
 
 @Slf4j
 @RestController
@@ -18,8 +23,24 @@ import javax.annotation.Resource;
 @RequestMapping("/Web/Students")
 public class ViolationController {
 
-    @Resource
+    @Autowired
     private ViolationService violationService;
+
+    @Autowired
+    private AdministratorService administratorService;
+
+    @ApiOperation(value = "违法信息新增")
+    @PostMapping("/violation")
+    public Result<String> addViolation(@RequestBody ViolationAddDto violationAddDto) {
+        Long currentId = BaseContext.getCurrentId();
+        log.info("当前操作人id：{}", currentId);
+
+        log.info("新增违法信息：{}", violationAddDto);
+        // 数据拷贝
+        Violation violation = BeanUtil.copyProperties(violationAddDto, Violation.class);
+        violationService.save(violation);
+        return Result.success("新增成功");
+    }
 
     @ApiOperation(value = "违法信息分页查询")
     @GetMapping("/violationsPage")
@@ -37,5 +58,32 @@ public class ViolationController {
         return Result.success(pageBean);
     }
 
+    @ApiOperation(value = "违法信息审核")
+    @PostMapping("/violationAudit")
+    public Result<String> violationAudit(@RequestBody Violation violation) {
+        //就是修改违法信息当中 的 状态字段
+        log.info("违法信息审核：{}", violation);
+        // 获取当前线程操作人 id
+        Long currentId = BaseContext.getCurrentId();
+        log.info("当前操作人id：{}", currentId);
+        LambdaQueryWrapper<Administrator> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(Administrator::getId, currentId);
 
+        Administrator administrator = administratorService.getOne(lambdaQueryWrapper);
+        //权限校验
+        if (administrator.getStatus().equals("0")){
+            violationService.updateById(violation);
+
+            return Result.success("审核成功");
+        }else{
+            return Result.error("权限不足");
+        }
+
+    }
+
+    @ApiOperation(value = "违法信息处理进度查看")
+    @GetMapping("/violationProgress")
+    public Result<PageBean> violationProgress(){
+        return null;
+    }
 }
---
+++ src/main/java/com/computer/bikeSupervision/pojo/dto/ViolationAddDto.java
@@ -0,0 +1,32 @@
+package com.computer.bikeSupervision.pojo.dto;
+
+import lombok.Data;
+
+@Data
+public class ViolationAddDto {
+    /**
+     * 车牌号
+     */
+    private String licencePlate;
+
+    /**
+     * 违法图片
+     */
+    private String image;
+
+    /**
+     * 违法原因（在想要不要存编号）
+     */
+    private String cause;
+
+    /**
+     * 所属学校
+     */
+    private String schoolName;
+
+    /**
+     * 违法路段
+     */
+    private String railway;
+
+}
