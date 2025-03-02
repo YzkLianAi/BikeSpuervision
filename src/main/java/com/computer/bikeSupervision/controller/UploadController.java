ef6cee55ef723fe64a2a97e479063ab700053ac1
YzkLianAi <2942834094@qq.com>
2025-03-02 14:29:15 +0800

    1.分离文件上传 图片上传 和 头像上传接口

--- src/main/java/com/computer/bikeSupervision/controller/UploadController.java
+++ src/main/java/com/computer/bikeSupervision/controller/UploadController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,20 +20,45 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 @Api(tags = "阿里云云端保存接口")
+@RequestMapping("/Upload")
 public class UploadController {
     @Autowired
     private AliOSSUtils aliOSSUtils;
 
     @ApiOperation(value = "上传图片至云端接口", notes = "需要传递一个MultipartFile 类型的文件")
-    @PostMapping("/upload")
-    public Result<String> upload(MultipartFile image) throws Exception {
+    @PostMapping("/uploadImage")
+    public Result<String> uploadImage(MultipartFile image) throws Exception {
         log.info("文件上传，文件名{}", image.getOriginalFilename());
 
         //调用阿里云OSS工具类进行上传
         String url = aliOSSUtils.upload(image);
         log.info("文件上传完成，文件访问路径url：{}", url);
 
-        return Result.success(url);
+        return Result.ok(url);
+    }
+
+    @ApiOperation(value = "上传文件至云端接口", notes = "需要传递一个MultipartFile 类型的文件")
+    @PostMapping("/uploadFile")
+    public Result<String> uploadFile(MultipartFile file) throws Exception {
+        log.info("文件上传，文件名{}", file.getOriginalFilename());
+
+        //调用阿里云OSS工具类进行上传
+        String url = aliOSSUtils.upload(file);
+        log.info("文件上传完成，文件访问路径url：{}", url);
+
+        return Result.ok(url);
+    }
+
+    @ApiOperation(value = "上传头像至云端接口", notes = "需要传递一个MultipartFile 类型的文件")
+    @PostMapping("/uploadHead")
+    public Result<String> uploadHead(MultipartFile head) throws Exception {
+        log.info("文件上传，文件名{}", head.getOriginalFilename());
+
+        //调用阿里云OSS工具类进行上传
+        String url = aliOSSUtils.upload(head);
+        log.info("文件上传完成，文件访问路径url：{}", url);
+
+        return Result.ok(url);
     }
 
     @ApiOperation(value = "上传图片至云端接口", notes = "需要传递一个MultipartFile 类型的文件")
