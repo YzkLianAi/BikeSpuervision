eabb9c15a122328a24e91ea84f30e9d22ffbc683
YzkLianAi <2942834094@qq.com>
2025-03-08 15:41:12 +0800

    refactor(config): 重命名 RedisConfiguration 为 RedisConfig
    -将 RedisConfiguration 类重命名为 RedisConfig，简化配置类名称
    - 在 RedisConfig 类中添加了详细的注释，解释了 RedisTemplate 的配置目的和使用方法
    - 优化了代码结构，提高了代码的可读性和可维护性

--- src/main/java/com/computer/bikeSupervision/config/RedisConfig.java
+++ src/main/java/com/computer/bikeSupervision/config/RedisConfig.java
@@ -11,11 +11,21 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @Slf4j
-public class RedisConfiguration {
+public class RedisConfig {
 
+    /**
+     * 配置 RedisTemplate 以定义键值对的序列化方式
+     * 该方法在 Spring 的 IoC 容器中定义了一个 Bean，类型为 RedisTemplate，用于操作 Redis 数据库
+     * RedisTemplate 提供了与 Redis 数据库交互的丰富 API，支持多种数据结构的操作
+     *
+     * @param redisConnectionFactory Redis 连接工厂，用于创建和管理 Redis 连接
+     * @return 返回一个配置好的 RedisTemplate 实例，用于后续的 Redis 操作
+     */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        // 创建 RedisTemplate 实例
         RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 设置连接工厂
         template.setConnectionFactory(redisConnectionFactory);
 
         // 使用 StringRedisSerializer 来序列化和反序列化 redis 的 key 值
@@ -30,6 +40,7 @@ public class RedisConfiguration {
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
         template.setValueSerializer(jackson2JsonRedisSerializer);
 
+        // 初始化 RedisTemplate
         template.afterPropertiesSet();
         return template;
     }
